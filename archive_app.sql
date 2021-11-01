b03edd605d74ef51a554602be3818df387bec5eb
Youness ARBOUH <youness.arbouh@dev.logigroup.ma>
2021-11-01 22:43:26 +0100

    db update

--- archive_app.sql
+++ archive_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : mar. 26 oct. 2021 à 22:55
+-- Généré le : lun. 01 nov. 2021 à 21:42
 -- Version du serveur : 8.0.27
 -- Version de PHP : 7.3.24-(to be removed in future macOS)
 
@@ -20,6 +20,141 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `archive_app`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `devise`
+--
+
+CREATE TABLE `devise` (
+  `id` int NOT NULL,
+  `name` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `devise`
+--
+
+INSERT INTO `devise` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'DH', '2021-10-30 22:58:05', '2021-10-30 22:58:05'),
+(2, 'Euro', '2021-10-30 22:58:11', '2021-10-30 22:58:11');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `marche_unique`
+--
+
+CREATE TABLE `marche_unique` (
+  `id` int NOT NULL,
+  `titulaire_id` int NOT NULL,
+  `societe_id` int NOT NULL,
+  `nature_operation_id` int NOT NULL,
+  `tfs_id` int NOT NULL,
+  `devise_id` int NOT NULL,
+  `num_marche` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type_marche` smallint NOT NULL,
+  `mode_passassion` smallint NOT NULL,
+  `fontionnement_investissement` varchar(1) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `annee_budgetaire` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `montant` double NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `line_budgetaire` int NOT NULL,
+  `document_passation` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `document_execution` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `marche_unique`
+--
+
+INSERT INTO `marche_unique` (`id`, `titulaire_id`, `societe_id`, `nature_operation_id`, `tfs_id`, `devise_id`, `num_marche`, `type_marche`, `mode_passassion`, `fontionnement_investissement`, `annee_budgetaire`, `montant`, `created_at`, `updated_at`, `line_budgetaire`, `document_passation`, `document_execution`) VALUES
+(1, 1, 2, 3, 1, 1, '01', 0, 0, 'F', '09', 20000, '2021-10-31 12:58:56', '2021-10-31 12:58:56', 222, '', ''),
+(2, 1, 2, 3, 1, 1, '01', 0, 0, 'F', '09', 20000, '2021-10-31 15:09:09', '2021-10-31 16:01:59', 1, 'adminlte-3-datatables-617eb195d8dfd662470651.csv', 'bienvenue-chez-logigroup-youness-617ebdf70ba70848432292.pdf');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `nature_operation_bon_commande`
+--
+
+CREATE TABLE `nature_operation_bon_commande` (
+  `id` int NOT NULL,
+  `code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nature_operation` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tfs_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `nature_operation_bon_commande`
+--
+
+INSERT INTO `nature_operation_bon_commande` (`id`, `code`, `nature_operation`, `tfs_id`) VALUES
+(1, '93', 'QSDK', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `nature_operation_contrat`
+--
+
+CREATE TABLE `nature_operation_contrat` (
+  `id` int NOT NULL,
+  `code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nature_operation` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tfs_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `nature_operation_contrat`
+--
+
+INSERT INTO `nature_operation_contrat` (`id`, `code`, `nature_operation`, `tfs_id`) VALUES
+(1, '08', 'AAA', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `nature_operation_marche_reconductible`
+--
+
+CREATE TABLE `nature_operation_marche_reconductible` (
+  `id` int NOT NULL,
+  `code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nature_operation` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tfs_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `nature_operation_marche_reconductible`
+--
+
+INSERT INTO `nature_operation_marche_reconductible` (`id`, `code`, `nature_operation`, `tfs_id`) VALUES
+(1, '03', 'marchés Reconductible', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `nature_operation_marche_unique`
+--
+
+CREATE TABLE `nature_operation_marche_unique` (
+  `id` int NOT NULL,
+  `code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nature_operation` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tfs_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `nature_operation_marche_unique`
+--
+
+INSERT INTO `nature_operation_marche_unique` (`id`, `code`, `nature_operation`, `tfs_id`) VALUES
+(3, '01', 'Test', 1);
 
 -- --------------------------------------------------------
 
@@ -59,7 +194,10 @@ CREATE TABLE `societe` (
 INSERT INTO `societe` (`id`, `titulaire_id`, `name`, `code_societe`, `adresse`, `created_at`, `updated_at`) VALUES
 (2, 1, '3f industrie', 0, '36, rue imam al boukhari maarif', '2021-10-26 16:58:14', '2021-10-26 16:58:14'),
 (3, 1, '3f industrie', 0, '36, rue imam al boukhari maarif', '2021-10-26 20:27:17', '2021-10-26 20:27:17'),
-(4, 1, 'TEST', 2, 'TEST', '2021-10-26 20:27:33', '2021-10-26 20:27:33');
+(4, 1, 'TESTa', 3, 'aTEST', '2021-10-26 20:27:33', '2021-10-30 23:32:00'),
+(5, 1, '3f industrie', 3, '36, rue imam al boukhari maarif', '2021-10-31 22:47:01', '2021-10-31 22:47:01'),
+(6, 1, '3f industrie', 1, '36, rue imam al boukhari maarif', '2021-10-31 22:47:35', '2021-10-31 22:47:35'),
+(7, 1, '3f industrie', 1, '36, rue imam al boukhari maarif', '2021-10-31 22:56:00', '2021-10-31 22:56:00');
 
 -- --------------------------------------------------------
 
@@ -80,7 +218,30 @@ CREATE TABLE `societe_titulaire` (
 
 INSERT INTO `societe_titulaire` (`id`, `name`, `created_at`, `updated_at`) VALUES
 (1, 'Marocaine', '2021-10-26 16:02:47', '2021-10-26 16:02:47'),
-(2, 'Francaise', '2021-10-26 21:01:19', '2021-10-26 21:01:19');
+(2, 'Etrangère', '2021-10-26 21:01:19', '2021-10-30 23:31:42');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tfs`
+--
+
+CREATE TABLE `tfs` (
+  `id` int NOT NULL,
+  `name` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `code` varchar(1) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `tfs`
+--
+
+INSERT INTO `tfs` (`id`, `name`, `code`, `created_at`, `updated_at`) VALUES
+(1, 'Service', 'S', '2021-10-30 23:35:05', '2021-10-30 23:35:05'),
+(2, 'Fourniture', 'F', '2021-10-30 23:35:05', '2021-10-30 23:35:05'),
+(3, 'Rest', 'R', '2021-10-30 23:09:38', '2021-10-30 23:09:45');
 
 -- --------------------------------------------------------
 
@@ -112,6 +273,51 @@ INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `roles`, `password
 --
 
 --
+-- Index pour la table `devise`
+--
+ALTER TABLE `devise`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `marche_unique`
+--
+ALTER TABLE `marche_unique`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_C28F6B6DA10273AA` (`titulaire_id`),
+  ADD KEY `IDX_C28F6B6DFCF77503` (`societe_id`),
+  ADD KEY `IDX_C28F6B6D28C42716` (`nature_operation_id`),
+  ADD KEY `IDX_C28F6B6DD7897FC9` (`tfs_id`),
+  ADD KEY `IDX_C28F6B6DF4445056` (`devise_id`);
+
+--
+-- Index pour la table `nature_operation_bon_commande`
+--
+ALTER TABLE `nature_operation_bon_commande`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_16AAA8BFD7897FC9` (`tfs_id`);
+
+--
+-- Index pour la table `nature_operation_contrat`
+--
+ALTER TABLE `nature_operation_contrat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_AF8F9019D7897FC9` (`tfs_id`);
+
+--
+-- Index pour la table `nature_operation_marche_reconductible`
+--
+ALTER TABLE `nature_operation_marche_reconductible`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_F8D79C30D7897FC9` (`tfs_id`);
+
+--
+-- Index pour la table `nature_operation_marche_unique`
+--
+ALTER TABLE `nature_operation_marche_unique`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_20342644D7897FC9` (`tfs_id`);
+
+--
 -- Index pour la table `reset_password_request`
 --
 ALTER TABLE `reset_password_request`
@@ -132,6 +338,12 @@ ALTER TABLE `societe_titulaire`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `tfs`
+--
+ALTER TABLE `tfs`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `user`
 --
 ALTER TABLE `user`
@@ -143,6 +355,42 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT pour la table `devise`
+--
+ALTER TABLE `devise`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `marche_unique`
+--
+ALTER TABLE `marche_unique`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `nature_operation_bon_commande`
+--
+ALTER TABLE `nature_operation_bon_commande`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `nature_operation_contrat`
+--
+ALTER TABLE `nature_operation_contrat`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `nature_operation_marche_reconductible`
+--
+ALTER TABLE `nature_operation_marche_reconductible`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `nature_operation_marche_unique`
+--
+ALTER TABLE `nature_operation_marche_unique`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT pour la table `reset_password_request`
 --
 ALTER TABLE `reset_password_request`
@@ -152,13 +400,19 @@ ALTER TABLE `reset_password_request`
 -- AUTO_INCREMENT pour la table `societe`
 --
 ALTER TABLE `societe`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT pour la table `societe_titulaire`
 --
 ALTER TABLE `societe_titulaire`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `tfs`
+--
+ALTER TABLE `tfs`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `user`
@@ -169,6 +423,40 @@ ALTER TABLE `user`
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `marche_unique`
+--
+ALTER TABLE `marche_unique`
+  ADD CONSTRAINT `FK_C28F6B6D28C42716` FOREIGN KEY (`nature_operation_id`) REFERENCES `nature_operation_marche_unique` (`id`),
+  ADD CONSTRAINT `FK_C28F6B6DA10273AA` FOREIGN KEY (`titulaire_id`) REFERENCES `societe_titulaire` (`id`),
+  ADD CONSTRAINT `FK_C28F6B6DD7897FC9` FOREIGN KEY (`tfs_id`) REFERENCES `tfs` (`id`),
+  ADD CONSTRAINT `FK_C28F6B6DF4445056` FOREIGN KEY (`devise_id`) REFERENCES `devise` (`id`),
+  ADD CONSTRAINT `FK_C28F6B6DFCF77503` FOREIGN KEY (`societe_id`) REFERENCES `societe` (`id`);
+
+--
+-- Contraintes pour la table `nature_operation_bon_commande`
+--
+ALTER TABLE `nature_operation_bon_commande`
+  ADD CONSTRAINT `FK_16AAA8BFD7897FC9` FOREIGN KEY (`tfs_id`) REFERENCES `tfs` (`id`);
+
+--
+-- Contraintes pour la table `nature_operation_contrat`
+--
+ALTER TABLE `nature_operation_contrat`
+  ADD CONSTRAINT `FK_AF8F9019D7897FC9` FOREIGN KEY (`tfs_id`) REFERENCES `tfs` (`id`);
+
+--
+-- Contraintes pour la table `nature_operation_marche_reconductible`
+--
+ALTER TABLE `nature_operation_marche_reconductible`
+  ADD CONSTRAINT `FK_F8D79C30D7897FC9` FOREIGN KEY (`tfs_id`) REFERENCES `tfs` (`id`);
+
+--
+-- Contraintes pour la table `nature_operation_marche_unique`
+--
+ALTER TABLE `nature_operation_marche_unique`
+  ADD CONSTRAINT `FK_20342644D7897FC9` FOREIGN KEY (`tfs_id`) REFERENCES `tfs` (`id`);
 
 --
 -- Contraintes pour la table `reset_password_request`
